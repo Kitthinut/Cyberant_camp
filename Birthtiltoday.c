2b0c1415ea9be9deb5c5023f2c70c127536a641f
Bam_Kitthinut <110830551+Kitthinut@users.noreply.github.com>
2023-10-15 22:27:10 +0700

    Assignment 2

---
+++ Birthtiltoday.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int date, month, year;
+    printf("Enter your birthday (dd mm yyyy): ");
+    scanf("%d %d %d", &date, &month, &year);
+    printf("Your Birthday %d %d %d", date, month, year);
+    printf("Enter today date: ");
+
+
+    /*1 year  = 12 months
+      1 month = 30 days ; 1,3,5,7,8,10,12: 31 days ; 4,6,9,11 : 30 days
+      feb = 28 days ; every 4 year : feb = 29 days
+      1 days  = 24 hrs
+      1 hrs   = 60 mins
+      1 mins  = 60 secs*/
+
+    float secpy =
+
+}
