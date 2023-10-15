ffb62a0ee218ea3a8cb9d40313b77a3f220148fd
Bam_Kitthinut <110830551+Kitthinut@users.noreply.github.com>
2023-10-15 22:15:11 +0700

    Lab 8

---
+++ Plus.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    printf("Enter num1: ");
+    scanf("%d", &a);
+    printf("Enter num2: ");
+    scanf("%d", &b);
+    int ans = a + b;
+    printf("Answer: %d", ans);
+}
